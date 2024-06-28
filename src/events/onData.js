7dc839291313e74ab9856ea4420efbdb059a7518
hyeonseol00 <hyeonseol22@naver.com>
2024-06-28 13:03:16 +0900

    ✨ update : 실제 패킷 처리부 추가

--- src/events/onData.js
+++ src/events/onData.js
@@ -1,4 +1,6 @@
 import { config } from '../config/config.js';
+import { PACKET_TYPE } from '../constants/header.js';
+import { packetParser } from '../utils/parser/packetParser.js';
 
 export const onData = (socket) => async (data) =>
 {
@@ -17,7 +19,19 @@ export const onData = (socket) => async (data) =>
 
 			console.log(`패킷 길이: ${length}`);
 			console.log(`패킷 타입: ${packetType}`);
-			console.log(packet);
+
+			switch (packetType)
+			{
+				case PACKET_TYPE.PING:
+					break;
+				case PACKET_TYPE.NORMAL:
+					const { handlerId, sequence, payload, userId } = packetParser(packet);
+
+					console.log('핸들러 ID:', handlerId);
+					console.log('유저 ID:', userId);
+					console.log('페이로드:', payload);
+					console.log('시퀀스:', sequence);
+			}
 		}
 		else
 		{
