ad9b8bd9d7c7f162f537d5cf66f8de49b077a0b6
hyeonseol00 <hyeonseol22@naver.com>
2024-06-28 12:28:57 +0900

    ✨ update : 데이터 수신 코드 추가

--- src/events/onData.js
+++ src/events/onData.js
@@ -1,4 +1,27 @@
-export const onData = (socket) => (data) =>
+import { config } from '../config/config.js';
+
+export const onData = (socket) => async (data) =>
 {
-	console.log(data);
+	socket.buffer = Buffer.concat([socket.buffer, data]);
+	const totalHeaderLength = config.packet.totalLength + config.packet.typeLength;
+
+	while (socket.buffer.length >= totalHeaderLength)
+	{
+		const length = socket.buffer.readUInt32BE(0);
+		const packetType = socket.buffer.readUInt8(config.packet.totalLength);
+
+		if (socket.buffer.length >= length)
+		{
+			const packet = socket.buffer.slice(totalHeaderLength, length);
+			socket.buffer = socket.buffer.slice(length);
+
+			console.log(`패킷 길이: ${length}`);
+			console.log(`패킷 타입: ${packetType}`);
+			console.log(packet);
+		}
+		else
+		{
+			break;
+		}
+	}
 };
