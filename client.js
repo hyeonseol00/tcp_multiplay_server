e8feb0070458555318952e61780cc210e2420165
hyeonseol00 <hyeonseol22@naver.com>
2024-06-28 12:29:58 +0900

    🍻 test : 테스트 클라이언트 갱신

--- client.js
+++ client.js
@@ -1,5 +1,25 @@
 import net from 'net';
 
+const TOTAL_LENGTH = 4; // 전체 길이를 나타내는 4바이트
+const PACKET_TYPE_LENGTH = 1; // 패킷타입을 나타내는 1바이트
+
+const readHeader = (buffer) =>
+{
+	return {
+		length: buffer.readUInt32BE(0),
+		packetType: buffer.writeUInt8(TOTAL_LENGTH),
+	};
+};
+
+const writeHeader = (length, packetType) =>
+{
+	const headerSize = TOTAL_LENGTH + PACKET_TYPE_LENGTH;
+	const buffer = Buffer.alloc(headerSize);
+	buffer.writeUInt32BE(length + headerSize, 0);
+	buffer.writeUInt8(packetType, TOTAL_LENGTH);
+	return buffer;
+};
+
 // 서버에 연결할 호스트와 포트
 const HOST = 'localhost';
 const PORT = 5555;
@@ -9,11 +29,28 @@ const client = new net.Socket();
 client.connect(PORT, HOST, () =>
 {
 	console.log('서버 연결 성공');
+
+	const message = 'Hi, There!';
+	const test = Buffer.from(message);
+
+	const header = writeHeader(test.length, 11);
+	const packet = Buffer.concat([header, test]);
+	client.write(packet);
 });
 
 client.on('data', (data) =>
 {
-	console.log(data);
+	const buffer = Buffer.from(data); // 버퍼 객체의 메서드를 사용하기 위해 변환
+
+	const { handlerId, length } = readHeader(buffer);
+	console.log(`핸들러 ID: ${handlerId}`);
+	console.log(`패킷 길이: ${length}`);
+
+	const headerSize = TOTAL_LENGTH + PACKET_TYPE_LENGTH;
+	// 메시지 추출
+	const message = buffer.slice(headerSize); // 앞의 헤더 부분을 잘라낸다.
+
+	console.log(`서버에게 받은 메세지: ${message}`);
 });
 
 client.on('close', () =>
