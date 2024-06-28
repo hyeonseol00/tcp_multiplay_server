d5b6a3331fb54b768e161afb58dd3eb8ca0db1c5
hyeonseol00 <hyeonseol22@naver.com>
2024-06-28 17:40:33 +0900

    🎉 add : 핸들러 추가

---
+++ src/handlers/user/initial.handler.js
@@ -0,0 +1,12 @@
+import { addUser } from '../../session/user.session.js';
+
+const initialHandler = async ({ socket, userId, payload }) =>
+{
+	const { deviceId } = payload;
+
+	addUser(socket, deviceId);
+
+	socket.write("");
+};
+
+export default initialHandler;
