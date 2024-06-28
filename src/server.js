c3411ccaa282719f462539b88b0f8869f2bdee98
hyeonseol00 <hyeonseol22@naver.com>
2024-06-28 12:01:09 +0900

    🎉 add : 서버 기초 작성

--- src/server.js
+++ src/server.js
@@ -1,1 +1,28 @@
-console.log("Hello!");
+import net from 'net';
+
+const PORT = 5555;
+
+const server = net.createServer((socket) =>
+{
+	console.log(`클라이언트가 연결되었습니다: ${socket.remoteAddress}:${socket.remotePort}`);
+	socket.on('data', (data) =>
+	{
+		console.log(data);
+	});
+
+	socket.on('end', () =>
+	{
+		console.log('연결 종료');
+	});
+
+	socket.on('error', (err) =>
+	{
+		console.error('소켓 에러:', err);
+	});
+});
+
+server.listen(PORT, () =>
+{
+	console.log(`서버가 ${PORT}번 포트로 동작중입니다!`);
+	console.log(server.address());
+});
