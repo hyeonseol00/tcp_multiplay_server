461998f688c38d2572fa8f2c76c4efe1ae0cf562
hyeonseol00 <hyeonseol22@naver.com>
2024-07-01 10:45:03 +0900

    ✨ update : 유저 세션에 클래스 적용

--- src/session/user.session.js
+++ src/session/user.session.js
@@ -1,8 +1,9 @@
 import { userSessions } from './sessions.js';
+import User from '../classes/models/user.class.js';
 
 export const addUser = (socket, uuid) =>
 {
-	const user = { socket, id: uuid, sequence: 0 };
+	const user = new User(uuid, socket);
 	userSessions.push(user);
 	return user;
 };
@@ -24,7 +25,7 @@ export const getNextSequence = (id) =>
 {
 	const user = getUserById(id);
 	if (user)
-		return ++user.sequence;
+		return user.getNextSequence();
 
 	return null;
 };
