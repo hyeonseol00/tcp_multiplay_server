51abf83769f4e7a03e7ece4116b6d084ca3673ab
hyeonseol00 <hyeonseol22@naver.com>
2024-07-01 11:28:32 +0900

    ✨ update : 게임 참가 핸들러 매핑

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,6 +1,7 @@
 import { HANDLER_IDS } from '../constants/handlerIds.js';
 import CustomError from '../utils/error/customError.js';
 import createGameHandler from './game/createGame.handler.js';
+import joinGameHandler from './game/joinGame.handler.js';
 import initialHandler from './user/initial.handler.js';
 
 const handlers = {
@@ -11,6 +12,10 @@ const handlers = {
 	[HANDLER_IDS.CREATE_GAME]: {
 		handler: createGameHandler,
 		protoType: 'game.CreateGamePayload',
+	},
+	[HANDLER_IDS.JOIN_GAME]: {
+		handler: joinGameHandler,
+		protoType: 'game.JoinGamePayload',
 	},
 };
 
