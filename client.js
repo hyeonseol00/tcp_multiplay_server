8f3c871dffbca629eef17a443d4edb721afa03a9
hyeonseol00 <hyeonseol22@naver.com>
2024-07-01 16:23:56 +0900

    🍻 test : 테스트 클라이언트 갱신

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 import net from 'net';
+import Long from 'long';
 import { getProtoMessages, loadProtos } from './src/init/loadProtos.js';
 
 const TOTAL_LENGTH = 4; // 전체 길이를 나타내는 4바이트
@@ -52,10 +53,30 @@ const sendPacket = (socket, packet) =>
 	socket.write(packetWithLength);
 };
 
+const sendPong = (socket, timestamp) =>
+{
+	const protoMessages = getProtoMessages();
+	const Ping = protoMessages.common.Ping;
+
+	const pongMessage = Ping.create({ timestamp });
+	const pongBuffer = Ping.encode(pongMessage).finish();
+	const packetLength = Buffer.alloc(TOTAL_LENGTH);
+	packetLength.writeUInt32BE(pongBuffer.length + TOTAL_LENGTH + PACKET_TYPE_LENGTH, 0);
+
+	const packetType = Buffer.alloc(PACKET_TYPE_LENGTH);
+	packetType.writeUInt8(0, 0);
+
+	const packetWithLength = Buffer.concat([packetLength, packetType, pongBuffer]);
+
+	socket.write(packetWithLength);
+};
+
 const HOST = 'localhost';
 const PORT = 5555;
 
 const client = new net.Socket();
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 client.connect(PORT, HOST, async () =>
 {
@@ -74,10 +95,10 @@ client.on('data', (data) =>
 
 	const packetType = data.readUInt8(4);
 	const packet = data.slice(totalHeaderLength, totalHeaderLength + length);
+	const protoMessages = getProtoMessages();
 
 	if (packetType === 1)
 	{
-		const protoMessages = getProtoMessages();
 		const Response = protoMessages.response.Response;
 
 		try
@@ -93,6 +114,25 @@ client.on('data', (data) =>
 		catch (err)
 		{
 			console.log(err);
+		}
+	}
+	else if (packetType === 0)
+	{
+		try
+		{
+			const Ping = protoMessages.common.Ping;
+			const pingMessage = Ping.decode(packet);
+			const timestampLong = new Long(
+				pingMessage.timestamp.low,
+				pingMessage.timestamp.high,
+				pingMessage.timestamp.unsigned,
+			);
+			console.log('서버로부터 ping을 수신했습니다:', timestampLong.toNumber());
+			sendPong(client, timestampLong.toNumber());
+		}
+		catch (err)
+		{
+			console.error('Ping 처리 중 오류 발생:', err);
 		}
 	}
 });
