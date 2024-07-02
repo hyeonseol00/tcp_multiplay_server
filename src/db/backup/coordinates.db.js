e1dd1a10c629d953acee403e1fe6b4378fb05dc8
hyeonseol00 <hyeonseol22@naver.com>
2024-07-02 20:21:48 +0900

    🎉 add : 좌표 백업 쿼리 세팅

---
+++ src/db/backup/coordinates.db.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+import pools from '../database.js';
+import { SQL_QUERIES } from './coordinates.queries.js';
+import { toCamelCase } from '../../utils/transformCase.js';
+
+export const findUserByDeviceID = async (deviceId) =>
+{
+	const [rows] = await pools.USER_COORDINATES.query(SQL_QUERIES.FIND_USER_BY_DEVICE_ID, [deviceId]);
+
+	return toCamelCase(rows[0]);
+};
+
+export const createUserbackupCoordinate = async (deviceId) =>
+{
+	const id = uuidv4();
+
+	await pools.USER_COORDINATES.query(SQL_QUERIES.BACKUP_COORDINATE, [id, deviceId, 0, 0]);
+
+	return { id, deviceId };
+};
+
+export const updateUserBackupCoordinate = async (id, x, y) =>
+{
+	await pools.USER_COORDINATES.query(SQL_QUERIES.UPDATE_COORDINATE, [x, y, id]);
+};
---
+++ src/db/backup/coordinates.queries.js
@@ -0,0 +1,5 @@
+export const SQL_QUERIES = {
+	FIND_USER_BY_DEVICE_ID: 'SELECT * FROM coordinates_backup WHERE device_id = ?',
+	BACKUP_COORDINATE: 'INSERT INTO coordinates_backup (id, device_id, x, y) VALUES (?, ?, ?, ?)',
+	UPDATE_COORDINATE: 'UPDATE coordinates_backup SET x = ?, y = ? WHERE id = ?',
+};
