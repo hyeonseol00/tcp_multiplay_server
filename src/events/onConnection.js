6d6cf140450161f5138e829aef78e4a5dd0c06f4
hyeonseol00 <hyeonseol22@naver.com>
2024-07-03 16:51:42 +0900

    🔥 del : 디버그 출력 코드 삭제

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -5,7 +5,6 @@ import { onData } from './onData.js';
 export const onConnection = (socket) =>
 {
 	console.log('클라이언트가 연결되었습니다:', socket.remoteAddress, socket.remotePort);
-	console.log("현재 접속 중인 유저: ", gameSession.getAllUserIds());
 
 	socket.buffer = Buffer.alloc(0);
 
